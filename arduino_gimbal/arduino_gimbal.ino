8c9775a3c1de6ef56d9fe3dfc2394e47c0117a10
TheNoobInventor <eduobijoro@mailfence.com>
2022-06-19 13:37:05 +0100

    Updated and add comments on sketch files

--- arduino_gimbal/arduino_gimbal.ino
+++ arduino_gimbal/arduino_gimbal.ino
@@ -1,12 +1,7 @@
 /* 
-<<<<<<< HEAD
  * A 3D printed DIY gimbal - fitted with 3 FS90 Micro servo motors - is controlled using an MPU6050 
  * inertial measurement unit (IMU) sensor in order to keep 3D printed platform stable during rotation in 
  * any axis. 
-=======
- * Arduino Nano + 3 servo90 (confirm this) motors + MPU6050
- * 
->>>>>>> b68a4675e36fb2ab535b0e128afabe0355ea8a3d
  * 
  * All of these components are connected to an Arduino Nano and powered via a 9V battery - stepped down 
  * to 5V using a buck converter.
@@ -32,13 +27,13 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // Interrupt detection routine
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
     mpuInterrupt = true;
 }
 
 #define INTERRUPT_PIN 2  
-MPU6050 mpu;
+MPU6050 accelgyro;      // Instantiate an MPU6050 object
 
 // Servo definitions
 Servo servoYaw;
@@ -46,107 +41,103 @@ Servo servoPitch;
 Servo servoRoll;
 
 void setup() {
-    Wire.begin();
-    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-   
-    // initialize serial communication
-    Serial.begin(38400);//
+  
+  // Initialize Wire library and join i2c bus
+  Wire.begin();
+  Wire.setClock(400000); 
+ 
+  // Initialize serial communication
+  Serial.begin(38400);
 
-    // Initialize device
-    Serial.println(F("Initializing I2C devices..."));
-    mpu.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
+  // Initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  accelgyro.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
 
-    // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  // Verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(accelgyro.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
+  // Load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = accelgyro.dmpInitialize();
 
-    // Offsets obtained from calibration file (mpu6050_offsets.ino)
-    mpu.setXGyroOffset(-209);
-    mpu.setYGyroOffset(-100); 
-    mpu.setZGyroOffset(-50); 
-    mpu.setZAccelOffset(-2153); 
+  // Set device offsets obtained from mpu6050_offsets.ino sketch
+  accelgyro.setXGyroOffset(-203);
+  accelgyro.setYGyroOffset(-95); 
+  accelgyro.setZGyroOffset(-48); 
+  accelgyro.setZAccelOffset(1933); 
+  
+  // Make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
     
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // Calibration Time: generate offsets and calibrate our MPU6050
-        mpu.CalibrateAccel(6);
-        mpu.CalibrateGyro(6);
-        mpu.PrintActiveOffsets();
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
+    // Calibrate and fine tune MPU6050 with the offsets set above
+    accelgyro.CalibrateAccel(6);
+    accelgyro.CalibrateGyro(6);
+    accelgyro.PrintActiveOffsets();
+    
+    // Turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    accelgyro.setDMPEnabled(true);
 
-        // enable Arduino interrupt detection
-        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-        Serial.println(F(")..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
+    // Enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = accelgyro.getIntStatus();
 
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
+    // Set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
 
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
+    // Get expected DMP packet size for later comparison
+    packetSize = accelgyro.dmpGetFIFOPacketSize();
+  } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+  }
 
-    // Attach servos to digital pins
-    servoYaw.attach(5);
-    servoPitch.attach(10);
-    servoRoll.attach(4);
+  // Attach servos to digital pins
+  servoYaw.attach(5);
+  servoPitch.attach(10);
+  servoRoll.attach(4);
 }
 
 void loop() {
 
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
-    // read a packet from FIFO
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
-        // display Euler angles in degrees
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  // If programming failed, don't try to do anything
+  if (!dmpReady) return;
+  
+  // Read a packet from FIFO
+  if (accelgyro.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+    
+    // Display Euler angles in degrees
+    accelgyro.dmpGetQuaternion(&q, fifoBuffer);
+    accelgyro.dmpGetGravity(&gravity, &q);
+    accelgyro.dmpGetYawPitchRoll(ypr, &q, &gravity);
 
-        // Convert Yaw, Pitch and Roll values from Radians to degrees
-        ypr[0] = ypr[0] * 180 / M_PI;
-        ypr[1] = ypr[1] * 180 / M_PI;
-        ypr[2] = ypr[2] * 180 / M_PI;
+    // Convert Yaw, Pitch and Roll values from Radians to degrees
+    ypr[0] = ypr[0] * 180 / M_PI;
+    ypr[1] = ypr[1] * 180 / M_PI;
+    ypr[2] = ypr[2] * 180 / M_PI;
 
-        /*
-        *
-        */    
-        // Reverse map value of servo if the servo moves
-        // in the same direction of motion
-        
-        int yawValue = map(ypr[0], -90, 90, 0, 180);            
-        int pitchValue = map(ypr[1], -90, 90, 180, 0);            
-        int rollValue = map(ypr[2], -90, 90, 180, 0);
+    /* Map the MPU6050 movement to the angular movements of the servo motors. This mapping has to be done for the servos
+       to move in the opposite direction of the MPU6050 accelerometer motion, for each respective axis, to attempt
+       stabilizing the gimbal platform. 
+    */
+    int yawValue = map(ypr[0], -90, 90, 0, 180);            
+    int pitchValue = map(ypr[1], -90, 90, 180, 0);            
+    int rollValue = map(ypr[2], -90, 90, 180, 0);
 
-        // Control servos according to the MPU6050 orientation
-        servoYaw.write(yawValue);
-        servoPitch.write(pitchValue);
-        servoRoll.write(rollValue);
-    }
-<<<<<<< HEAD
+    // Control servos according to the MPU6050 orientation
+    servoYaw.write(yawValue);
+    servoPitch.write(pitchValue);
+    servoRoll.write(rollValue);
+  }
 }
-=======
-      
-    ypr[0] = ypr[0] - correct[0];
-    ypr[1] = ypr[1] - correct[1];
-    ypr[2] = ypr[2] - correct[2];
-  }  
->>>>>>> b68a4675e36fb2ab535b0e128afabe0355ea8a3d
--- docs/index.md
+++ docs/index.md
@@ -1,20 +1,63 @@
 # Diy Arduino Gimbal
 
-(To be completed)
+A 3D printed do-it-yourself (DIY) gimbal - fitted with 3 FS90 Micro servo motors - is controlled using an MPU6050 inertial measurement unit (IMU) sensor in order to keep the 3D printed platform stable during rotation in any axis. The servo motors and the IMU sensor are connected to the Arduino Nano microcontroller.
+
 ## Hardware
 
 ### Components
-This includes tools and misc like double sided tape
-Pictures of some components/sensors
 
-### Assembly
+The following components (and tools) were used in this project:
+
+- 3D printed gimbal parts
+
+- Arduino Nano (and usb cable)
+
+- MPU6050
+
+- 3 x FS90 Micro servo motors
+
+- LM2596 Buck Regulator
+
+- 9V battery
+
+- 2.54 mm female and male headers
+
+- Loose cables to solder on female/male headers
+
+- Veroboard to solder on female headers (for the arduino nano and MPU6050) and
+male headers for the servo cables
+
+- On/Off switch
+
+- Soldering iron
+
+- Screwdriver set
+
+- Double sided tape
+
+- Duck tape
+
+### Project Wiring
+
+The diagram below shows how the electronic components of the gimbal are connected.
 
 <p float="center">
   <img src=images/gimbal_wiring_diagram.png width="400">
 </p>
 
+<p float="center">
+  <img src=images/components.png width="400">
+</p>
+
+
 Notes about assembly. Some observations too
 Pictures of the assembled project
+
+stl files for the project...add links
+
+why you need buck Regulator?
+
+uses for the double sided tape and the duck tape
 
 ## Software
 
@@ -24,6 +67,10 @@ Software used
 ### Software install
 how to download and run the code
 
+libraries used
+
+
+## MPU6050
 
 ### MPU6050 Calibrations
 
